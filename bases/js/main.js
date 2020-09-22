8fe05419e171c81c2c30dd7bcaff653332bd538c
DawsonDerycke <dawsonderycke@hotmail.com>
2020-09-22 21:18:10 +0200

    ajouts des tags

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,2 +1,35 @@
+/*
+
 console.log('Mon message');
 console.error('Mon message erreur');
+
+document.addEventListener('click', function(){
+    alert('MESSAGE');
+});
+
+document.getElementById
+document.querySelector('')
+
+document.getElementByClassName
+document.getElementByName
+document.getElementByTagName
+document.querySelectorAll()
+
+*/
+
+const button = document.getElementById('bouton-bleu');
+
+if(button != null){
+button.addEventListener('click', function(){
+    alert('TEST');
+}); 
+}
+const btns = document.getElementsByClassName('btn');
+
+btns[0].addEventListener('click', function(){
+    alert('MESSAGE');
+});
+
+const table = document.querySelector('table');
+
+table.style.display = 'none';
