467b3caa67f54bca9ce5c93e1c7752933bfaff9b
DawsonDerycke <dawsonderycke@hotmail.com>
2020-10-06 21:12:03 +0200

    car + road

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,30 +1,44 @@
-// console.log('Mon message');
-// console.error('Mon message erreur');
-//document.addEventListener('click', function() {
-//    alert('MESSAGE');
-//});
-
-/*const myButton = document.getElementById('bouton-bleu');
-if(myButton != null) {
-    myButton.addEventListener('click', function() {
-       const table = document.querySelector('table');
-       table.style.display = 'none';
-    });
-}*/
-
-/*$('#bouton-bleu').on('click', function() {
-    $('table').find('tr').last().remove();
-});*/
-
-const btn = document.getElementById('bouton-bleu');
-btn.addEventListener('click', function() {
-    document.querySelectorAll('table tr').deleteRow(-1);
-});
-
-
-/*const btns = document.getElementsByClassName('btn');
-
-btns[0].addEventListener('click', function(){
+/*
+console.log('Mon message');
+console.error('Mon message erreur');
+document.addEventListener('click', function(){
     alert('MESSAGE');
 });
+document.getElementById
+document.querySelector('')
+document.getElementByClassName
+document.getElementByName
+document.getElementByTagName
+document.querySelectorAll()
 */
+
+/*$('#bouton-bleu').on('click', function(){
+    $('table').find('tr').last().remove();
+});
+*/
+
+const button = document.getElementById('bouton-bleu');
+const table = document.querySelector('table');
+const btns = document.getElementsByClassName('btn');
+
+/*btns[0].addEventListener('click', function(){
+    alert('MESSAGE');
+    table.style.display = '';
+});*/
+
+button.addEventListener('click', function(){
+    if(table.style.display == 'none'){
+        table.style.display = 'table';
+    }
+    else{
+        table.style.display = 'none';
+    }
+});
+
+const button2 = document.getElementById('bouton-deux');
+button2.addEventListener('click', function(){
+    const taille = document.getElementsByTagName('tr').length;
+    for(i = i; i < taille; i++){
+        document.querySelectorAll('tr')[i].lastElementChild.innerHTML = '';
+    }
+});
---
+++ car/main.js
@@ -0,0 +1,11 @@
+const img = document.getElementById('car');
+
+setInterval(function(){
+    img.style.marginTop ="50px";
+}, 500);
+setInterval(function(){
+    img.style.margin ="0px";
+}, 800);
+setInterval(function(){
+    img.style.marginBottom ="50px";
+}, 1300);
