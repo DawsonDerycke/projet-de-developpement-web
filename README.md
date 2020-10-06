e0860c4a8f9517e669ec770293647c9a98e42d93
DawsonDerycke <dawsonderycke@hotmail.com>
2020-10-06 18:27:31 +0200

    Autre PC

---
+++ README.md
@@ -0,0 +1,24 @@
+# Projet de développement web
+
+1. [Les bases pour bien démarrer](documentation/bases.md)
+
+2. Inscription Github, Netlify et installation de NodeJS, git
+
+3. [Manipulation de DOM et event listener](documentation/manipulation_dom.md)
+
+4. Initiation à GIT et NPM
+
+5. Installation des librairies
+
+6. Manipulation du DOM
+
+7. les évenements HTML
+
+8. Requêtes ajax
+
+9. Déployer son projet sur Netlify
+
+
+
+
+
---
+++ bases/js/main.js
@@ -0,0 +1,30 @@
+// console.log('Mon message');
+// console.error('Mon message erreur');
+//document.addEventListener('click', function() {
+//    alert('MESSAGE');
+//});
+
+/*const myButton = document.getElementById('bouton-bleu');
+if(myButton != null) {
+    myButton.addEventListener('click', function() {
+       const table = document.querySelector('table');
+       table.style.display = 'none';
+    });
+}*/
+
+/*$('#bouton-bleu').on('click', function() {
+    $('table').find('tr').last().remove();
+});*/
+
+const btn = document.getElementById('bouton-bleu');
+btn.addEventListener('click', function() {
+    document.querySelectorAll('table tr').deleteRow(-1);
+});
+
+
+/*const btns = document.getElementsByClassName('btn');
+
+btns[0].addEventListener('click', function(){
+    alert('MESSAGE');
+});
+*/
